0ad30b7228bb3eb4067f770b62c15be71ec86a1f
tokoyax <tokoyax.dev@gmail.com>
2018-06-12 16:20:39 +0900

    storeの設定を追加

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import CalculatorContainer from './containers/CalculatorContainer';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './reducers'
+import './index.css';
 
-ReactDOM.render(
-  <CalculatorContainer />,
+const store = createStore(reducer);
+
+render(
+  <Provider store={store}>
+    <CalculatorContainer />
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
